f08979b1ea9d728803258b65ff9b91396fc8fd82
skrymsli <skrymsli@gmail.com>
2019-09-30 16:44:14 -0600

    Refactor

---
+++ Generator.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace seqgen
+{
+    internal class Generator
+    {
+        private const string IndexVariable = "%i";
+        private int StartIndex => _zeroBased ? 0 : 1;
+
+        private bool _zeroBased;
+        public Generator WithZeroBased(bool zeroBased = true)
+        {
+            _zeroBased = zeroBased;
+            return this;
+        }
+
+        private static string _separator = ",";
+        public Generator WithSeparator(string sep)
+        {
+            if (sep != null)
+            {
+                _separator = sep;
+            }
+
+            return this;
+        }
+
+        private static int? _firstColumnWithTemplate;
+        public void Generate(string[] templates, TextWriter output, int itemCount)
+        {
+            var indexList = new List<ColumnInfo>();
+            for(int i = 0; i < templates.Length; i++)
+            {
+                indexList.Add(new ColumnInfo(templates[i], StartIndex));
+                if (_firstColumnWithTemplate == null && indexList[i].Include)
+                {
+                    _firstColumnWithTemplate = i;
+                }
+            }
+
+            if (_firstColumnWithTemplate == null)
+            {
+                output.WriteLine(EmitLine(indexList));
+            }
+            else while (indexList[_firstColumnWithTemplate.Value].Count < (_zeroBased ? itemCount : itemCount + 1))
+            {
+                output.WriteLine(EmitLine(indexList));
+                Increment(itemCount, indexList);
+            }
+        }
+
+        private static string EmitLine(IReadOnlyList<ColumnInfo> indexList)
+        {
+            var sb = new StringBuilder();
+            for(int i = 0; i < indexList.Count; i++)
+            {
+                var field = indexList[i].Template;
+                if (indexList[i].Include)
+                {
+                    field = field.Replace(IndexVariable, indexList[i].Count.ToString());
+                }
+                
+                sb.Append(field);
+                if(i < indexList.Count - 1) sb.Append(_separator);
+            }
+
+            return sb.ToString();
+        }
+
+        private void Increment(int max, IList<ColumnInfo> indexList)
+        {
+            var firstColumn = _firstColumnWithTemplate.GetValueOrDefault(0);
+            for (int i = indexList.Count - 1; i >= firstColumn; i--)
+            {
+                if (!indexList[i].Include) continue;
+
+                if (i == firstColumn || indexList[i].Count < (_zeroBased ? max - 1 : max))
+                {
+                    indexList[i].Count += 1;
+                    return;
+                }
+                
+                indexList[i].Count = StartIndex;
+            }
+        }
+
+        private class ColumnInfo
+        {
+            public ColumnInfo(string template, int startIndex)
+            {
+                Template = template;
+                Count = startIndex;
+                Include = Template.Contains(IndexVariable);
+
+            }
+            public string Template { get; }
+            public int Count {get; set; }
+            public bool Include { get; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,39 +8,60 @@ namespace seqgen
 {
     internal class Program
     {
-        private static int StartIndex => _zeroBased ? 0 : 1;
-
-        private static bool _zeroBased;
-
         private const string ZeroFlag = "-zero";
+        private static bool _zeroBased;
         private const string SeparatorFlag = "-sep";
-        private static string _separator = ",";
-        private const string IndexVariable = "%i";
-
-        private static int? FirstColumnWithTemplate = null;
-
-        private class ColumnInfo
-        {
-            public ColumnInfo(string template)
-            {
-                Template = template;
-                Count = StartIndex;
-                Include = Template.Contains(IndexVariable);
-
-            }
-            public string Template { get; }
-            public int Count {get; set; }
-            public bool Include { get; }
-        }
+        private static string _separator;
 
         private static void Main(string[] args)
+        {
+            try
+            {
+                args = ParseZeroFlag(args);
+                args = ParseSeparator(args, out _separator);
+
+                if (args.Length == 0)
+                {
+                    throw new UsageException("Too few arguments.");
+                }
+
+                if (!int.TryParse(args[0], out var itemCount))
+                {
+                    throw new UsageException("Missing item count.");
+                }
+                args = args.Skip(1).ToArray();
+                if (args.Length == 0)
+                {
+                    throw new UsageException("Missing fields.");
+                }
+                
+
+                var generator = new Generator()
+                    .WithSeparator(_separator)
+                    .WithZeroBased(_zeroBased);
+                generator.Generate(args, Console.Out, itemCount);
+            }
+            catch (UsageException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Usage();
+            }
+
+        }
+
+        private static string[] ParseZeroFlag(string[] args)
         {
             if (args.Any(x => x == ZeroFlag))
             {
                 _zeroBased = true;
-                args = args.Except(new[] {ZeroFlag}).ToArray();
+                return args.Except(new[] {ZeroFlag}).ToArray();
             }
+            return args;
+        }
 
+        private static string[] ParseSeparator(string[] args, out string separator)
+        {
+            separator = ",";
             if (args.Any(x => x == SeparatorFlag))
             {
                 for (int i = 0; i < args.Length; i++)
@@ -49,94 +70,56 @@ namespace seqgen
                     {
                         if (i + 1 > args.Length)
                         {
-                            Usage();
-                            return;
+                            throw new UsageException("Missing separator.");
                         }
 
-                        _separator = args[i + 1];
+                        separator = args[i + 1];
                     }
                 }
-                args = args.Except(new[] {SeparatorFlag, _separator}).ToArray();
+                return args.Except(new[] {SeparatorFlag, separator}).ToArray();
             }
 
-            if (args.Length == 0)
-            {
-                Usage();
-                return;
-            }
-            if (!int.TryParse(args[0], out var itemCount))
-            {
-                Usage();
-                return;
-            }
-
-            var arglen = args.Length - 1;
-            if (arglen == 0)
-            {
-                Usage();
-                return;
-            }
-
-            args = args.Skip(1).ToArray();
-            var indexList = new List<ColumnInfo>();
-            for(int i = 0; i < args.Length; i++)
-            {
-                indexList.Add(new ColumnInfo(args[i]));
-                if (FirstColumnWithTemplate == null && indexList[i].Include)
-                {
-                    FirstColumnWithTemplate = i;
-                }
-            }
-
-            if (FirstColumnWithTemplate == null)
-            {
-                Console.WriteLine(EmitLine(indexList));
-            }
-            else while (indexList[FirstColumnWithTemplate.Value].Count < (_zeroBased ? itemCount : itemCount + 1))
-            {
-                Console.WriteLine(EmitLine(indexList));
-                Increment(itemCount, indexList);
-            }
+            return args;
         }
+
 
         private static void Usage()
         {
-            Console.WriteLine("seqgen [-zero] [-sep <separator>] <count> <field1> <field2> ... <fieldN>");
+            Console.WriteLine(UsageText());
         }
 
-        private static string EmitLine(IReadOnlyList<ColumnInfo> indexList)
+        private static string UsageText()
         {
-            var sb = new StringBuilder();
-            for(int i = 0; i < indexList.Count; i++)
-            {
-                var field = indexList[i].Template;
-                if (indexList[i].Include)
-                {
-                    field = field.Replace(IndexVariable, indexList[i].Count.ToString());
-                }
-                
-                sb.Append(field);
-                if(i < indexList.Count - 1) sb.Append(_separator);
-            }
+            return @"seqgen: Generate sequential data rows
 
-            return sb.ToString();
+Usage: seqgen [-zero] [-sep <separator>] <count> <field1> <field2> ... <fieldN>
+
+Required:
+  count: The number of times to iterate on each field.
+  field: Text to output. At least one field is required.
+
+Optional:
+  -zero: Use zero based counters. Default: Use one-based counters.
+  -sep: Use alternate field separator. Default: ,
+
+Use %i in field names to generate sequential counters.
+
+Example:
+
+  seqgen 2 Row%i Data%i
+
+  This generates the following data:
+
+   Row1,Data1
+   Row1,Data2
+   Row2,Data1
+   Row2,Data2
+";
         }
+    }
 
-        private static void Increment(int max, IList<ColumnInfo> indexList)
-        {
-            var firstColumn = FirstColumnWithTemplate.GetValueOrDefault(0);
-            for (int i = indexList.Count - 1; i >= firstColumn; i--)
-            {
-                if (!indexList[i].Include) continue;
-
-                if (i == firstColumn || indexList[i].Count < (_zeroBased ? max - 1 : max))
-                {
-                    indexList[i].Count += 1;
-                    return;
-                }
-                
-                indexList[i].Count = StartIndex;
-            }
-        }
+    internal class UsageException : Exception
+    {
+        public UsageException(string message) : base(message) {}
     }
 }
